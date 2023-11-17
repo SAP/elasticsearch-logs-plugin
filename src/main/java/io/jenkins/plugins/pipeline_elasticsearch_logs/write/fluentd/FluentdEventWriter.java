14906a618f910e17f020f5794943b8cf5bd7a3d1
parsa vali <parsa.vali@sap.com>
2023-11-17 00:12:14 -0800

    Run fluentd.emitData in a separate thread
    As the main threads are affected by the Durable tasks and interrupted
    exceptions can occur due to long sleeps it was decided to run the
    fluentd.emitData in a separate thread.
    
    Initialized new newCachedThreadPool which can re-use the thread in case
    there exists an idle thread in the threadpool, otherwise creates a new
    thread and inject it to the threadpool. As thread creation is costly
    re-using threads helps the performance.

--- src/main/java/io/jenkins/plugins/pipeline_elasticsearch_logs/write/fluentd/FluentdEventWriter.java
+++ src/main/java/io/jenkins/plugins/pipeline_elasticsearch_logs/write/fluentd/FluentdEventWriter.java
@@ -6,6 +6,12 @@ import static java.lang.String.format;
 import java.io.IOException;
 import java.time.Instant;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
@@ -14,7 +20,6 @@ import java.util.logging.Logger;
 
 import javax.annotation.Nonnull;
 
-import org.komamitsu.fluency.BufferFullException;
 import org.komamitsu.fluency.EventTime;
 import org.komamitsu.fluency.Fluency;
 import org.komamitsu.fluency.RetryableException;
@@ -31,6 +36,7 @@ public class FluentdEventWriter implements EventWriter {
 
     private transient Fluency fluentd;
     private transient FluentdErrorHandler errorHandler;
+    private ExecutorService executor;
 
     FluentdEventWriterConfig config;
 
@@ -68,6 +74,8 @@ public class FluentdEventWriter implements EventWriter {
         fluentd = builder.build(config.getHost(), config.getPort());
         LOGGER.finer(format("Created new %s, host=%s, port=%s, hashCode=%s",
                 fluentd, config.getHost(), config.getPort(), fluentd.hashCode()));
+        LOGGER.finer("Created new cached thread pool object");
+        executor = Executors.newCachedThreadPool();
     }
 
     /**
@@ -90,46 +98,103 @@ public class FluentdEventWriter implements EventWriter {
 
     private void emitData(String tag, Map<String, Object> data) throws IOException {
         LOGGER.log(Level.FINEST, "Emitting log event: {0}", new Object[] { data });
-
-        EventTime eventTime = getEventTime(data);
-        int timeoutMillis = config.getEmitTimeoutMillis();
-        IOException lastException = null;
-        long startTimeNanos = System.nanoTime();
         long retryCount = 0;
+        boolean threadInterrupted = false;
+        Future<Void> future = executor.submit(new EmitDataTask(tag, data));
 
-        while (true) {
-            try {
-                fluentd.emit(tag, eventTime, data);
-                long elapsedTimeNanos = System.nanoTime() - startTimeNanos;
-                LOGGER.log(Level.FINEST, "Log event emitted after {0} nanoseconds", new Object[] { elapsedTimeNanos });
-                break;
-            } catch (BufferFullException ex) {
-                lastException = ex;
-            } catch (IOException ex) {
-                LOGGER.log(Level.INFO, "Exception occurred while emitting data: {0}", ex);
-            }
-
-            long elapsedMillis = (System.nanoTime() - startTimeNanos) / 1_000_000;
-            if (timeoutMillis >= 0 && elapsedMillis > timeoutMillis) {
-                throw new IOException("Failed to emit log event after " + timeoutMillis + " milliseconds. Giving up.", lastException);
+        try {
+            while(!future.isDone() && !future.isCancelled()) {
+                try {
+                    LOGGER.log(Level.INFO, "Waiting for the EmitDataTask completion ...");
+                    TimeUnit.MICROSECONDS.sleep(100);
+                } catch (InterruptedException ex) {
+                    threadInterrupted = true;
+                }
             }
 
             try {
-                long delayMicros = (long) (10_000.0 * Math.pow(1.3, (double) Math.min(retryCount, 18)));
-                TimeUnit.MICROSECONDS.sleep(delayMicros);
-            } catch (InterruptedException ex) {
-                LOGGER.log(Level.FINEST, "Exception occurred while sleeping: {0}", ex);
+                if (future.isDone() || future.isCancelled()) {
+                    future.get();
+                }
+            } catch (CancellationException | ExecutionException | InterruptedException ex) {
+                LOGGER.log(Level.INFO, "Exception occurred in the 'EmitDataTask' thread: {0}", ex);
             }
-            retryCount++;
+        } finally {
+            if (threadInterrupted) {
+                // restore interrupted flag
+                Thread.currentThread().interrupt();
+                retryCount = 0;
+                while(!Thread.interrupted() && retryCount < 100) {
+                    Thread.currentThread().interrupt();
+                    retryCount++;
+                }
+            }
         }
     }
 
-    private EventTime getEventTime(Map<String, Object> data) {
-        Instant instant = Instant.parse((String) data.get(TIMESTAMP));
-        long epochSeconds = instant.getEpochSecond();
-        long nanoSeconds = instant.getNano();
-        return EventTime.fromEpoch(epochSeconds, nanoSeconds);
+    private class EmitDataTask implements Callable<Void> {
+        private EventTime eventTime;
+        private int timeoutMillis = config.getEmitTimeoutMillis();
+        private IOException lastException = null;
+        private long startTimeNanos = System.nanoTime();
+        private Map<String, Object> data;
+        private String tag;
+        private boolean threadInterrupted = false;
+        private long retryCount = 0;
+        private long threadInterruptionRetryCount = 0;
+
+        EmitDataTask(String tag, Map<String, Object> data) {
+            this.data = data;
+            this.tag = tag;
+            eventTime = getEventTime(data);
+        }
+
+        public Void call() throws InterruptedException, IOException {
+            try {
+                while (true) {
+                    try {
+                        fluentd.emit(tag, eventTime, data);
+                        long elapsedTimeNanos = System.nanoTime() - startTimeNanos;
+                        LOGGER.log(Level.FINEST, "Log event emitted after {0} nanoseconds", new Object[] { elapsedTimeNanos });
+                        break;
+                    } catch (IOException ex) {
+                        lastException = ex;
+                    }
+
+                    long elapsedMillis = (System.nanoTime() - startTimeNanos) / 1_000_000;
+                    if (timeoutMillis >= 0 && elapsedMillis > timeoutMillis) {
+                        throw new IOException("Failed to emit log event after " + timeoutMillis + " milliseconds. Giving up.", lastException);
+                    }
+
+                    try {
+                        long delayMicros = (long) (10_000.0 * Math.pow(1.3, (double) Math.min(this.retryCount, 18)));
+                        TimeUnit.MICROSECONDS.sleep(delayMicros);
+                    } catch (InterruptedException ex) {
+                        this.threadInterrupted = true;
+                    }
+                    this.retryCount++;
+                }
+            } finally {
+                if (this.threadInterrupted) {
+                    Thread.currentThread().interrupt();
+                    threadInterruptionRetryCount = 0;
+                    while(!Thread.interrupted() && threadInterruptionRetryCount < 100) {
+                        Thread.currentThread().interrupt();
+                        threadInterruptionRetryCount++;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private EventTime getEventTime(Map<String, Object> data) {
+            Instant instant = Instant.parse((String) data.get(TIMESTAMP));
+            long epochSeconds = instant.getEpochSecond();
+            long nanoSeconds = instant.getNano();
+            return EventTime.fromEpoch(epochSeconds, nanoSeconds);
+        }
     }
+
 
     @Override
     public void close() throws IOException
@@ -164,6 +229,8 @@ public class FluentdEventWriter implements EventWriter {
         }
         finally {
             this.lock.writeLock().unlock();
+            LOGGER.log(Level.INFO, "Terminated the thread pool");
+            this.executor.shutdown();
         }
     }
 
