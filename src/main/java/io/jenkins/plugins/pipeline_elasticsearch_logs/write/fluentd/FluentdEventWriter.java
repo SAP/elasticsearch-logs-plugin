fb5a46d462064172c76a6cf2088954ca6e45b510
valiparsa <63588437+valiparsa@users.noreply.github.com>
2023-12-08 09:56:19 +0100

    Run emitting steps of the fluency library in a separate thread (#108)
    Threads might get interrupted when stopped by the
    'workflow-durable-task-step-plugin'. Such interruptions does not allow
    fluency to properly flush the queued events in the buffer.
    java.io.IOException: Failed to move retention buffer due to interruption
    
    See interruptions in the 'workflow-durable-task-step-plugin' plugin.
    https://github.com/jenkinsci/workflow-durable-task-step-plugin/blob/master/src/main/java/org/jenkinsci/plugins/workflow/steps/durable_task/DurableTaskStep.java#L392
    https://github.com/jenkinsci/workflow-support-plugin/blob/master/src/main/java/org/jenkinsci/plugins/workflow/support/concurrent/Timeout.java#L86
    
    As the main threads are affected by the Durable tasks and interrupted
    exceptions can occur due to long sleeps it was decided to run the
    fluentd.emitData in a separate thread.
    
    Initialized new newCachedThreadPool which can re-use the thread in case
    there exists an idle thread in the threadpool, otherwise creates a new
    thread and inject it to the threadpool. As thread creation is costly
    re-using threads helps the performance.
    
    
    Co-authored-by: Andreas Brehmer <andreas.brehmer@sap.com>

--- src/main/java/io/jenkins/plugins/pipeline_elasticsearch_logs/write/fluentd/FluentdEventWriter.java
+++ src/main/java/io/jenkins/plugins/pipeline_elasticsearch_logs/write/fluentd/FluentdEventWriter.java
@@ -6,6 +6,11 @@ import static java.lang.String.format;
 import java.io.IOException;
 import java.time.Instant;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
@@ -30,6 +35,8 @@ public class FluentdEventWriter implements EventWriter {
 
     private transient Fluency fluentd;
     private transient FluentdErrorHandler errorHandler;
+
+    private ExecutorService executor = Executors.newCachedThreadPool();
 
     FluentdEventWriterConfig config;
 
@@ -90,77 +97,202 @@ public class FluentdEventWriter implements EventWriter {
     private void emitData(String tag, Map<String, Object> data) throws IOException {
         LOGGER.log(Level.FINEST, "Emitting log event: {0}", new Object[] { data });
 
-        EventTime eventTime = getEventTime(data);
-        int timeoutMillis = config.getEmitTimeoutMillis();
-        IOException lastException = null;
-        long startTimeNanos = System.nanoTime();
-        long retryCount = 0;
-
-        while (true) {
-            try {
-                fluentd.emit(tag, eventTime, data);
-                long elapsedTimeNanos = System.nanoTime() - startTimeNanos;
-                LOGGER.log(Level.FINEST, "Log event emitted after {0} nanoseconds", new Object[] { elapsedTimeNanos });
-                break;
-            } catch (IOException ex) {
-                lastException = ex;
+        boolean isThreadInterrupted = false;
+        try {
+            // run Fluency.emit() in another thread to protect it from
+            // thread interrupts by Jenkins.
+            Future<Void> future = executor.submit(new FluencyEmitTask(tag, data));
+            while (true) {
+                try {
+                    future.get(); // block until task is finished
+                    break;
+                }
+                catch (InterruptedException ex) {
+                    // remember that the thread has been interrupted,
+                    // but still wait for completion of the emit task
+                    isThreadInterrupted = true;
+                }
+                catch (ExecutionException ex) {
+                    Throwable cause = ex.getCause();
+                    if (cause instanceof IOException) {
+                        throw (IOException)cause;
+                    }
+                    throw new RuntimeException(cause);
+                }
             }
-
-            long elapsedMillis = (System.nanoTime() - startTimeNanos) / 1_000_000;
-            if (timeoutMillis >= 0 && elapsedMillis > timeoutMillis) {
-                throw new IOException("Failed to emit log event after " + timeoutMillis + " milliseconds. Giving up.", lastException);
-            }
-
-            try {
-                long delayMicros = (long) (10_000.0 * Math.pow(1.3, (double) Math.min(retryCount, 18)));
-                TimeUnit.MICROSECONDS.sleep(delayMicros);
-            } catch (InterruptedException ex) {
+        }
+        finally {
+            if (isThreadInterrupted) {
+                // restore thread's interrupted status
                 Thread.currentThread().interrupt();
             }
-            retryCount++;
         }
     }
 
-    private EventTime getEventTime(Map<String, Object> data) {
-        Instant instant = Instant.parse((String) data.get(TIMESTAMP));
-        long epochSeconds = instant.getEpochSecond();
-        long nanoSeconds = instant.getNano();
-        return EventTime.fromEpoch(epochSeconds, nanoSeconds);
+    /**
+     * A tasks that emits data via Fluency.<p/>
+     *
+     * It is supposed to be executed in a separate thread to protect it from
+     * thread interrupts by Jenkins. Fluency has a bug that leads to loss of
+     * buffered event data in case of thread interrupt.
+     */
+    private class FluencyEmitTask implements Callable<Void> {
+        private String tag;
+        private Map<String, Object> data;
+        private EventTime eventTime;
+
+        FluencyEmitTask(String tag, Map<String, Object> data) {
+            this.data = data;
+            this.tag = tag;
+            eventTime = getEventTime(data);
+        }
+
+        public Void call() throws IOException {
+            final int timeoutMillis = config.getEmitTimeoutMillis();
+            final long startTimeNanos = System.nanoTime();
+
+
+            boolean isThreadInterrupted = false;
+            try {
+                IOException lastException = null;
+                long retryCount = 0;
+                while (true) {
+                    try {
+                        fluentd.emit(tag, eventTime, data);
+                        long elapsedTimeNanos = System.nanoTime() - startTimeNanos;
+                        LOGGER.log(Level.FINEST, "Log event emitted after {0} nanoseconds", new Object[] { elapsedTimeNanos });
+                        break;
+                    } catch (IOException ex) {
+                        lastException = ex;
+                    }
+
+                    long elapsedMillis = (System.nanoTime() - startTimeNanos) / 1_000_000;
+                    if (timeoutMillis >= 0 && elapsedMillis > timeoutMillis) {
+                        throw new IOException("Failed to emit log event after " + timeoutMillis + " milliseconds. Giving up.", lastException);
+                    }
+
+                    try {
+                        // Use exponential back-off that grows slowly and is
+                        // capped at a not too high value so that the retry
+                        // rate is high enough to prevent longer delays.
+                        // It's expected that Fluency's buffers run full
+                        // frequently and retrying must happen here without
+                        // adding much delay.
+                        long delayMicros = (long) (10_000.0 * Math.pow(1.3, (double) Math.min(retryCount, 18)));
+                        TimeUnit.MICROSECONDS.sleep(delayMicros);
+                    } catch (InterruptedException ex) {
+                        isThreadInterrupted = true;
+                    }
+                    retryCount++;
+                }
+            } finally {
+                if (isThreadInterrupted) {
+                    // restore thread's interrupted status
+                    Thread.currentThread().interrupt();
+                }
+            }
+            return null;
+        }
+
+        private EventTime getEventTime(Map<String, Object> data) {
+            Instant instant = Instant.parse((String) data.get(TIMESTAMP));
+            long epochSeconds = instant.getEpochSecond();
+            long nanoSeconds = instant.getNano();
+            return EventTime.fromEpoch(epochSeconds, nanoSeconds);
+        }
     }
 
     @Override
-    public void close() throws IOException
+    public void close() throws Exception
     {
         try {
             this.lock.writeLock().lock();
             failIfClosed();
 
-            if (fluentd != null) {
-                try {
-                    fluentd.flush();
+            boolean isThreadInterrupted = false;
+            try {
+                // run Fluency.emit() in another thread to protect it from
+                // thread interrupts by Jenkins.
+                Future<Void> future = executor.submit(new FluencyShutdownTask());
+                while (true) {
                     try {
-                        if (!fluentd.waitUntilAllBufferFlushed(config.getMaxWaitSecondsUntilBufferFlushed())) {
-                            throw new IOException("Not all data could be flushed.");
-                        }
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
+                        future.get(); // block until task is finished
+                        break;
                     }
-                    fluentd.close();
-                    try {
-                        if (!fluentd.waitUntilFlusherTerminated(config.getMaxWaitSecondsUntilFlusherTerminated())) {
-                            throw new IOException("Flusher not terminated.");
-                        }
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
+                    catch (InterruptedException ex) {
+                        // remember that the thread has been interrupted,
+                        // but still wait for completion of the emit task
+                        isThreadInterrupted = true;
                     }
-                } finally {
-                    fluentd.clearBackupFiles();
+                    catch (ExecutionException ex) {
+                        Throwable cause = ex.getCause();
+                        if (cause instanceof Exception) {
+                            throw (Exception)cause;
+                        }
+                        throw new RuntimeException(cause);
+                    }
                 }
-                checkForRetryableException();
+            } finally {
+                if (isThreadInterrupted) {
+                    // restore thread's interrupted status
+                    Thread.currentThread().interrupt();
+                }
             }
+
+            checkForRetryableException();
         }
         finally {
+            this.executor.shutdown();
+            this.executor = null;
+            this.fluentd = null;
             this.lock.writeLock().unlock();
+        }
+    }
+
+    /**
+     * A tasks that shuts down Fluency.<p/>
+     *
+     * It is supposed to be executed in a separate thread to protect it from
+     * thread interrupts by Jenkins. Fluency has a bug that leads to loss of
+     * buffered event data in case of thread interrupt.
+     */
+    private class FluencyShutdownTask implements Callable<Void> {
+
+        @Override
+        public Void call() throws Exception {
+            boolean isThreadInterrupted = false;
+            try {
+                fluentd.flush();
+                try {
+                    if (!fluentd.waitUntilAllBufferFlushed(config.getMaxWaitSecondsUntilBufferFlushed())) {
+                        throw new Exception("Not all data could be flushed.");
+                    }
+                } catch (InterruptedException e) {
+                    isThreadInterrupted = true;
+                }
+
+                fluentd.close();
+                try {
+                    if (!fluentd.waitUntilFlusherTerminated(config.getMaxWaitSecondsUntilFlusherTerminated())) {
+                        throw new Exception("Flusher not terminated.");
+                    }
+                } catch (InterruptedException e) {
+                    isThreadInterrupted = true;
+                }
+            } finally {
+                try {
+                    fluentd.clearBackupFiles();
+                }
+                catch (Throwable t) {
+                    LOGGER.fine("Failed to clear Fluency backup files: " + t.getMessage());
+                }
+
+                if (isThreadInterrupted) {
+                    // restore thread's interrupted status
+                    Thread.currentThread().interrupt();
+                }
+            }
+            return null;
         }
     }
 
